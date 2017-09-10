f1c9d3f5c514b201016f1f901922d4dcdd814e58
Haoxueren <haoxueren@foxmail.com>
2017-09-10 22:34:15 +0800

    生成单词地图

--- generate.py
+++ generate.py
@@ -1,9 +1,10 @@
 import os
+from urllib import parse
 
 input=open('C:/Users/Haoxueren/Desktop/WordBank/README.MD','w')
 for file in os.listdir('C:/Users/Haoxueren/Desktop/WordBank/Words'):
     word=file[:-4]
-    word_md='[%s](https://github.com/%s) | '%(word,word)
+    word_md='[%s](https://raw.githubusercontent.com/Haoxueren/English/master/Words/%s.png) | '%(word,parse.quote(word))
     input.write(word_md)
 input.close()
 print('Generate Completed...')
