bf175599381a651174fccfce1bfc476cae02ddd2
Haoxueren <haoxueren@foxmail.com>
2017-09-10 22:43:19 +0800

    generate.py中使用相对路径。

--- generate.py
+++ generate.py
@@ -1,8 +1,8 @@
 import os
 from urllib import parse
 
-input=open('C:/Users/Haoxueren/Desktop/WordBank/README.MD','w')
-for file in os.listdir('C:/Users/Haoxueren/Desktop/WordBank/Words'):
+input=open('README.MD','w')
+for file in os.listdir('Words'):
     word=file[:-4]
     word_md='[%s](https://raw.githubusercontent.com/Haoxueren/English/master/Words/%s.png) | '%(word,parse.quote(word))
     input.write(word_md)
