47b7820c187e6ef1ba06d299b0dbeefc534b1536
haoxueren <haoxueren@foxmail.com>
2019-11-04 20:31:14 +0800

    共1449个单词

--- Words/generate.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for file in os.listdir('C:/Users/Haoxueren/Desktop/WordBank/Words'):
-    word=file[:-4]
-    word_md='[%s](https://github.com/%s)'%(word,word)
-    file=open('')
-    print(word_md)
